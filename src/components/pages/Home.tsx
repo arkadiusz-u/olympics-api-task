ecedd9a934f239874980627330d1432419db8903
Arkadiusz Urbański <arkadiusz.u@icloud.com>
2026-05-05 00:16:39 +0200

    added reference files and shadcn component library

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { OlympicsApiScheduleUnit } from "../../lib/types";
+
+export default function Home({ data }: { data: OlympicsApiScheduleUnit[] }) {
+  return (
+    <div>
+      <h1>Olympics Football Matches</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.competitors[0].name} - {item.competitors[1].name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
